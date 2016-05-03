50bf9999c0e9d87e3fc106de398af8225c549136
Jake Ross <jirhiker@gmail.com>
2016-05-03 15:59:53 -0600

    updated urls

--- status/urls.py
+++ status/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from status import views
 
 root = 'labs/argon/labspy/'
+root = ''
 urlpatterns = [
     # url(r'^$', views.index, name='status_index'),
     # url(r'^graph/$', views.graph, name='status_graph_index'),
